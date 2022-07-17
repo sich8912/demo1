a7c812908b93ba988d48324d7e30447d74463207
siche <s-chebudm.net@mail.ru>
2022-07-17 11:10:50 +0400

    первая версия

--- src/main/java/com/example/demo/SimpleKafkaExampleApplication.java
+++ src/main/java/com/example/demo/SimpleKafkaExampleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 
+//комит типо
 @EnableKafka
 @SpringBootApplication
 public class SimpleKafkaExampleApplication {
